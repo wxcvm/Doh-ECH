0fabfb678e3e4d57a876cb3822f61039143179ab
wxcvm <133561272+wxcvm@users.noreply.github.com>
2026-08-20 14:02:14 +0000

    sync: 上游同步 f30c514

--- _worker.js
+++ _worker.js
@@ -84,12 +84,12 @@ const BUILTIN_HINTS = [
         noA: true, noAAAA: false
     },
     {
-        // Wikipedia 维基百科(禁用ipv4)（全球最大的百科知识库，其通用任播边缘全面部署了支持 HTTP/3 的 ATS 架构）
+        // Wikipedia 维基百科(实验性支持 HTTP/3 )
         domains: ["*.wikipedia.org", "*.wikimedia.org", "*.wikibooks.org", "*.wikidata.org"],
         ips: [],  noA: true, noAAAA: false
     },
    //Others(禁用ipv4)
-    { domains: ["*.docker.com", "*.docker.io", "*.production.cloudflare.docker.com"], ips: [], noA: true, noAAAA: false },       
+    { domains: ["*.docker.com","*.onlyfans.com", "*.docker.io", "*.production.cloudflare.docker.com"], ips: [], noA: true, noAAAA: false },       
     //FastlyCDN优化(禁用ipv6)
     { domains: ["*.reddit.com", "*.redd.it", "*.redditmedia.com", "*.redditstatic.com"], ips: ["151.101.1.140", "151.101.65.140", "151.101.129.140", "151.101.193.140"], noA: false, noAAAA: true },
     { domains: ["*.imgur.com", "*.i.imgur.com", "*.api.imgur.com", "*.s.imgur.com"], ips: ["151.101.1.193", "151.101.65.193", "151.101.129.193", "151.101.193.193"], noA: false, noAAAA: true },
